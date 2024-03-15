4ff9451d29ccb03b5c1f38d0385b8db57bdf9a09
dreisama <145562163+dreisama@users.noreply.github.com>
2024-03-15 17:58:32 +0800

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import TodoList from './Props/ToDo'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TodoList/>
-  </React.StrictMode>,
-)
